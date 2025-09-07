ea4b65552b2da2bacecb347eb5c02db7576be217
dkillam05 <danekillam@gmail.com>
2025-09-07 09:52:43 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // === Bump this on every release to bust the cache ===
-const CACHE_VERSION = 'df-v3.11-fresh';
+const CACHE_VERSION = 'df-v3.12-fresh';
 
 // Build correct base path (works on GitHub Pages like /USERNAME/REPO/)
 const SW_URL = new URL(self.location.href);
