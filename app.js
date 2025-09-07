a6f44ee973ff716b76a4871d3ca0a8ec2f96f6fe
dkillam05 <danekillam@gmail.com>
2025-09-07 10:01:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ===== App constants =====
-const APP_VERSION = 'v3.20';  // bump this each release (e.g. 'v3.9' or 'v3.9.0')
+const APP_VERSION = 'v3.21';  // bump this each release (e.g. 'v3.9' or 'v3.9.0')
 
 // ===== Routes =====
 const ROUTES = {
