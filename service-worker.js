516e9f414b92daff36d3de088d846164601ed104
dkillam05 <danekillam@gmail.com>
2025-09-08 16:57:56 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // === Bump on every release ===
-const CACHE_VERSION = 'df-v10.10-hot';
+const CACHE_VERSION = 'df-v10.11-hot';
 
 // Precache only the static shell (DO NOT include app.js/styles.css)
 const CORE = [
