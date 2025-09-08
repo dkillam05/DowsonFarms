58853f1324b2724004c8577b4e969e5dae7e05dd
dkillam05 <danekillam@gmail.com>
2025-09-07 19:29:10 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // === Bump this on every release to bust the cache ===
-const CACHE_VERSION = 'df-v7.3-fresh';
+const CACHE_VERSION = 'df-v7.4-fresh';
 
 // Build a base path that works on GitHub Pages and root hosting
 const SCOPE_PATH = new URL(self.registration.scope).pathname.replace(/\/?$/, '/');
