f293376419c1a73358511a3adca66476d1d8a961
dkillam05 <danekillam@gmail.com>
2025-09-10 23:35:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@
   if (window.__DF_V12_P1__) return; window.__DF_V12_P1__ = true;
 
   // Version surfaces in footer
-  const VERSION = 'v12.2.1';
+  const VERSION = 'v12.2.2';
 
   // App constants
   const APP = {
