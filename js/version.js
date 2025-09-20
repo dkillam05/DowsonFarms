b84f8cbca3e20e462edc1423ba592dffa84ca139
dkillam05 <danekillam@gmail.com>
2025-09-20 16:43:24 -0500

    Update version.js

--- js/version.js
+++ js/version.js
@@ -1,5 +1,5 @@
 // Dowson Farms — version.js
-window.APP_VERSION = "v8.0.0";
+window.APP_VERSION = "v8.0.1";
 
 document.addEventListener("DOMContentLoaded", () => {
   const v = document.getElementById("version");
