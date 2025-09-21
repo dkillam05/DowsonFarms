f274119599eb3725a36617b0050fec644ad421d5
dkillam05 <danekillam@gmail.com>
2025-09-21 06:35:10 -0500

    Update version.js

--- js/version.js
+++ js/version.js
@@ -5,7 +5,7 @@
            : (typeof window !== 'undefined') ? window
            : {};
 
-  root.APP_VERSION = "v8.0.3";   // <- bump here only
+  root.APP_VERSION = "v8.0.4";   // <- bump here only
 
   // Only touch the DOM if we’re in a page (not in SW)
   if (typeof document !== 'undefined') {
