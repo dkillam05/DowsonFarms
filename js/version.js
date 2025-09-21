c55d05957db9c2e03dea832c051b0364908de74b
dkillam05 <danekillam@gmail.com>
2025-09-21 10:36:01 -0500

    Update version.js

--- js/version.js
+++ js/version.js
@@ -5,7 +5,7 @@
            : {};
 
   // bump here only
-  root.APP_VERSION = "v9.1.1";
+  root.APP_VERSION = "v9.2.0";
   root.DF_VERSION  = root.APP_VERSION; // <— core.js expects this
 
   // Update DOM if we’re in a page (not in SW)
