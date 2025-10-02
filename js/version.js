57c85ebda731545482dadfd820903bac49eb2969
dkillam05 <danekillam@gmail.com>
2025-10-02 14:31:31 -0500

    Update version.js

---
+++ js/version.js
@@ -0,0 +1,3 @@
+// Single source of truth for the app version shown in the drawer (and anywhere else).
+// Bump this value whenever you ship a change you want visible to users.
+window.DF_VERSION = "0.1.0";
