c32628b8b130963445aecb0f83cc0bd0c392dd52
dkillam05 <danekillam@gmail.com>
2025-10-02 11:23:37 -0500

    Create redirector.js

---
+++ js/redirector.js
@@ -0,0 +1,12 @@
+// js/redirector.js
+// Force all shared links to return to main index.html
+
+(function () {
+  const here = location.pathname.split("/").pop();
+
+  // Allow index.html to load normally
+  if (!here || here === "index.html") return;
+
+  // Redirect everything else back to home
+  location.replace("/DowsonFarms/index.html");
+})();
